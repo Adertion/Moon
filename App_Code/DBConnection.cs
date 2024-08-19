7e10c961217bb10dfd5cc447af2d0be0879e5c14
Adertion <108845856+Adertion@users.noreply.github.com>
2024-08-19 20:26:35 +0800

    Add files via upload

---
+++ App_Code/DBConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+///DBConnection 的摘要说明
+///@author fmm 2015-06-10
+/// </summary>
+public class DBConnection
+{
+    public DBConnection()
+    {
+        //
+        //TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    protected static void ConnectSQL(SqlConnection conn)
+    {
+        if (conn.State == ConnectionState.Closed)
+        {
+            conn.Open();
+        }
+        else if (conn.State == ConnectionState.Broken)
+        {
+            conn.Close();
+            conn.Open();
+        }
+    }
+}
---
+++ App_Code/Sample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
+/// <summary>
+///Sample 的摘要说明
+/// </summary>
+public class Sample
+{
+    public Sample()
+    {
+    }
+
+   
+}
